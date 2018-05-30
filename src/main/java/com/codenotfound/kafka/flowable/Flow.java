98a807bfea69f18afba3591919c4f95902b4c4c7
Kevin Wu <kevin.wu@magicwindow.cn>
2018-05-30 19:36:03 +0800

    init commit

---
+++ src/main/java/com/codenotfound/kafka/flowable/Flow.java
@@ -0,0 +1,65 @@
+package com.codenotfound.kafka.flowable;
+
+/**
+ * Created by kevin on 30/05/2018.
+ */
+public interface Flow {
+}
+
+
+interface Stage {
+    void process(Event event);
+}
+
+interface Event {
+
+}
+
+interface EventSubscriber {
+    void onEvent(Event event);
+}
+
+interface EventEmitter {
+    void emit(Event event);
+}
+
+
+class FlowStage implements Stage {
+
+    @Override
+    public void process(Event event) {
+
+    }
+}
+
+class DeferredFlowStage implements Stage, EventEmitter {
+
+    @Override
+    public void emit(Event event) {
+
+    }
+
+    @Override
+    public void process(Event event) {
+
+    }
+
+}
+
+class SubscriberFlowStage implements Stage, EventSubscriber {
+
+    @Override
+    public void onEvent(Event event) {
+
+    }
+
+    @Override
+    public void process(Event event) {
+
+    }
+}
+
+
+
+
+
